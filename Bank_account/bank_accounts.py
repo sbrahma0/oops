26a4f46582fa3140cc7fc612e22ef03839bf4fe7
sbrahma0 <sbrahma0@gmail.com>
2023-09-04 18:54:00 -0700

    adding new funcions

--- Bank_account/bank_accounts.py
+++ Bank_account/bank_accounts.py
@@ -4,7 +4,15 @@ class BakAccount:
         self.name = acctName
 
         # f before the "" means its an f string, used if we intent to have names and decimal values in the print statement
-        # \n - is for next line
+        # \n - called a line break,is for next line
         # we specify variables with curly braces inside the string
         # :.2f - tells a 2 decimal place
         print(f"\n Account for {self.name} created \n with an ammount of ${self.balance:.2f} \n")
+
+    def getBalance(self):
+        print(f"\n Account for {self.name} has \n a balance of ${self.balance:.2f} \n")
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"\n Account for {self.name} \n deposit ammount = ${amount:.2f} \n new balance = ${self.balance:.2f} \n")
+    
--- Bank_account/main.py
+++ Bank_account/main.py
@@ -1,4 +1,7 @@
 from bank_accounts import *
 
-sayan = BakAccount(1000, "Sayan")
+sayan = BakAccount(1000, "sayan")
 kri = BakAccount(2000, "KB")
+
+sayan.getBalance()
+sayan.deposit(5000)
